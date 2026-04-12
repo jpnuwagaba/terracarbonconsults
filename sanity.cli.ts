0725cc1351b3e8fd1076259c7a33b05839a25af1
John Peter Nuwagaba <jnu@sucafina.com>
2026-04-12 16:54:47 +0300

    2nd rseolve error attempt

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,6 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+import {dataset, projectId} from './sanity/env'
 
 export default defineCliConfig({ api: { projectId, dataset } })
--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,16 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-04-11'
 
+const defaultDataset = 'terracarbonconsults-content'
+const defaultProjectId = 'z6pxm1h7'
+
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET || defaultDataset,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || defaultProjectId,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
